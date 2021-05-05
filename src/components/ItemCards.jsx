bf64800002ee6adb0a69342204ba4ea671b3da19
unknown <81273310+ykay220@users.noreply.github.com>
2021-05-04 21:30:27 -0400

    added banner css

--- src/components/ItemCards.jsx
+++ src/components/ItemCards.jsx
@@ -7,13 +7,21 @@ function ItemCards() {
  
 
     return (
+        <div className="mains">
+            <div className="banner">
+                {/* <img src="https://i.pinimg.com/originals/82/fd/a3/82fda3eadc5bc1ef2181fd0c38ec2593.jpg" alt=""/> */}
+            </div>
         <div className="main">
-
+        <div className="featured-div">
+            <h1>FEATURED CARS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nemo eum illo sunt illum, deserunt rerum nostrum placeat odit quo aliquam. Harum, eligendi architecto incidunt ipsum ratione recusandae doloribus consequatur!</p>
+        </div>
         
         <div className="items">
             {cardinfo.map(card=>
                 <Item key={card.id} card={card}/>
                 )}
+        </div>
         </div>
         </div>
         
